365a2cb9c3ac68ff3c1d163fcccbca71347184a4
root <felipeoliveramantero@gmail.com>
2024-04-21 11:08:30 -0700

    tasks

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -6,5 +6,4 @@ from pymongo import MongoClient
 
 def list_all(mongo_collection):
     """list all documents"""
-    list_documents = list(mongo_collection.find())
-    return list_documents
+    return list(mongo_collection.find())
