d95690fe1c79ef1ec0c870e81567fcd8ac01962e
Teby4 <felipeoliveramantero@gmail.com>
2024-04-24 06:50:58 -0700

    tasks

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+"""task 10"""
 
 from pymongo import MongoClient
 
 
 def update_topics(mongo_collection, name, topics):
+    """task 10"""
     return mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+"""task 9"""
 
 from pymongo import MongoClient
 
 
 def insert_school(mongo_collection, **kwargs):
+    """task 9"""
     return mongo_collection.insert_one(kwargs)
