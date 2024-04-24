1564a1732a4c24a1ac3fda8820f320aada7813bb
Teby4 <felipeoliveramantero@gmail.com>
2024-04-24 07:01:11 -0700

    task

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-"""task 3"""
+"""This module provides a function for calculating the floor of a floating-point number."""
 
 from math import floor as math_floor
 
 
 def floor(n: float) -> float:
-    """floor function"""
+    """Calculate the floor of a floating-point number.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        float: The largest integer less than or equal to `n`.
+
+    """
     return math_floor(n)
