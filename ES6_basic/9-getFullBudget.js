e9ac4c88dcd7405c3d059fbdf508c46b1099451b
Teby4 <felipeoliveramantero@gmail.com>
2024-05-08 09:35:09 -0700

    task

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,10 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  return {
+    ...budget,
+    getIncomeInDollars: `$${income}`,
+    getIncomeInEuros: `${income} euros`,
+  };
+}
