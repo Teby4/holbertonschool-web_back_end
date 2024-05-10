6f98f23caf0b01b81a9ecbb1db89fee174dc8512
root <felipeoliveramantero@gmail.com>
2024-05-09 19:05:07 -0700

    tasks

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
