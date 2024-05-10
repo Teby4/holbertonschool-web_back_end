f368cb0a7c9c33e8b9e3f7a9677b6af58c3da56d
root <felipeoliveramantero@gmail.com>
2024-05-09 18:39:21 -0700

    task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+	constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new Error('amount must be a number');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'string') {
+      throw new Error('Amount must be a string');
+    }
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new Error('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new Error('conversionRate must be a number');
+    }
+    return this.amount * conversionRate;
+  }
+}
