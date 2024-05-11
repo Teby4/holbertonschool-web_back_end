950b166eba43e16fb0969008943cda3059f49802
root <felipeoliveramantero@gmail.com>
2024-05-11 15:08:13 -0700

    task

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResult, userResult]) => {
-      console.log(photoResult.body, userResult.firstName, userResult.lastName);
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
     })
     .catch(() => {
       console.log('Signup system offline');
