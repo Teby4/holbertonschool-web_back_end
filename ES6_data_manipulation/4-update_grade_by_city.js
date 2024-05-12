64a86ebc4e4f7eef94e1a7932615c6aa37a9d056
root <felipeoliveramantero@gmail.com>
2024-05-11 21:08:14 -0700

    tasks

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
 export default function updateStudentGradeByCity(listStudents, city, newGrades) {
   // Filter students based on the provided city
-  const studentsCity = listStudents.filter(student => student.location === city);
+  const studentsCity = listStudents.filter((student) => student.location === city);
 
-  const updatedStudents = studentsCity.map(student => {
-    const studentGrade = newGrades.find(grade => grade.studentId === student.id);
+  const updatedStudents = studentsCity.map((student) => {
+    const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
     const grade = studentGrade ? studentGrade.grade : 'N/A';
     return { ...student, grade };
   });
