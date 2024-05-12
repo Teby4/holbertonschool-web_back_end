a264ea303cad6d14c0f15f1fcacfc408376c89c0
root <felipeoliveramantero@gmail.com>
2024-05-11 18:00:23 -0700

    task

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,4 @@
 export default function getStudentIdsSum(ListStudents) {
-  const sum = ListStudents.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue.id;
-  }, 0);
+  const sum = ListStudents.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
   return sum;
 }
