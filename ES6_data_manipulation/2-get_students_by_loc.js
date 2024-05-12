4e36c1c1fa2a392ee953db7683a481e9506baf9c
root <felipeoliveramantero@gmail.com>
2024-05-11 17:42:29 -0700

    task

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,4 @@
+import getListStudents from './0-get_list_students';
 export default function getStudentsByLocation(getListStudents, city) {
   return getListStudents().filter((student) => student.location === city);
 }
